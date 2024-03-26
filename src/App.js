ecd83b4f16795c57692014965d3f5e4ed98f894e
JoSungHyeon <jsh950618@gmail.com>
2024-03-26 16:20:09 +0900

    chapter1 commit

--- src/App.js
+++ src/App.js
@@ -5,36 +5,94 @@ import { useState } from 'react';
 function App() {
 
   let [listName, setListName] = useState(['1번 제목', '2번 제목', '3번 제목']);
-  let [favNum, setFavNum] = useState(0);
+  let [favNum, setFavNum] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [inputText, setInputText] = useState("");
 
-  function favPlus() {
-    setFavNum(favNum++);
-  }
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>Blog</h4>
       </div>
 
-      <button onClick={()=>{
+      {/* <button onClick={()=>{
         let copy = [...listName];
         copy[0] = '1번 제목 수정'
         setListName(copy)
       }}>글수정</button>
       <div className='list'>
         <h4>{listName[0]} <span onClick={() => setFavNum(favNum+1)} >👍</span> {favNum} </h4>
-        <p>{new Date().toLocaleDateString()}</p>
+        <p>2월 17일 발행</p>
       </div>
       <div className='list'>
         <h4>{listName[1]}</h4>
-        <p>{new Date().toLocaleDateString()}</p>
+        <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{listName[2]}</h4>
-        <p>{new Date().toLocaleDateString()}</p>
-      </div>
+        <h4 onClick={()=>{setModal(!modal)}} >{listName[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+      
+      {
+        listName.map((function(a, i) {
+          return (
+            <div className='list' key={i} value={i}>
+            <h4 onClick={()=>{
+              setModal(!modal)
+              setTitle(i)
+            }}>
+              { listName[i] } <span onClick={(e) => {
+              e.stopPropagation();
+              let copy = [...favNum];
+              copy[i] = copy[i] + 1;
+              setFavNum(copy)
+            }} >👍</span> {favNum[i]} </h4>
+            <p>2월 17일 발행</p>
+            <button
+              onClick={()=>{
+                let deleteData = [...listName];
+                deleteData.splice(i, 1);
+                setListName(deleteData);
+              }}
+            >글 삭제</button>
+          </div>)
+        }))
+      } 
+
+      <input onChange={(e)=>{
+        setInputText(e.target.value);
+      }}
+      value={inputText}
+      />
+      <button
+        onClick={()=> {
+          setListName([inputText, ...listName]);
+          setInputText("")
+        }}
+      >추가</button>
+
+      
+
+      {
+        modal == true
+        ? <Modal listName={listName} title={title} />
+        : null
+      }
+      
     </div>
   );
+}
+
+function Modal({listName, title}) {
+  return (
+    <div className='modal'>
+      <h4>{listName[title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button>글수정</button>
+    </div>
+  )
 }
 
 export default App;
